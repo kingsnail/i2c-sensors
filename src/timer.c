cf94a98b81d6de2e6acf8095b8c42025cb562cb3
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 18:26:28 +0100

    Update timer.c

--- src/timer.c
+++ src/timer.c
@@ -12,8 +12,20 @@ void* timer_thread_function(void* arg) {
     ts.tv_nsec = INTERVAL_MS * 1000000L;  // Convert ms to ns
 
     while(1) {
-        printf("Tick\n" );
+        printf("Tick\n");
+    	frameCounter++;
+	    if ( frameCounter > 10000 ) {
+		       frameCounter = 0;
+	    }
         nanosleep(&ts, NULL);
     }
     return NULL;
 }
+
+
+
+    while ( 1 ) {
+
+    	minorCount = 0;
+    	osDelay(0);
+    }
