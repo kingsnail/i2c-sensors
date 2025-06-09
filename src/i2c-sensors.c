8525b794d9ca68881d4afe082cf726099d60f2f7
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 17:21:10 +0100

    Update i2c-sensors.c

--- src/i2c-sensors.c
+++ src/i2c-sensors.c
@@ -1,7 +1,36 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "globals.h"
 
-int main( void ) {
-    printf("Hello World\r\n");
-    return( 0 );
+void* thread_function(void* arg) {
+    int thread_id = *((int*)arg);
+    for (int i = 0; i < 5; i++) {
+        printf("Thread %d: iteration %d\n", thread_id, i);
+        sleep(1);  // simulate some work
+    }
+    return NULL;
+}
+
+int main() {
+    const int NUM_THREADS = 4;
+    pthread_t threads[NUM_THREADS];
+    int thread_ids[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_ids[i] = i;
+        if (pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]) != 0) {
+            perror("Failed to create thread");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("All threads completed.\n");
+    return 0;
 }
