676921c04ddedfc5ca26bd485ed7ed0bdec46d9f
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 22:02:30 +0100

    Create i2c-utils.h

---
+++ inc/i2c-utils.h
@@ -0,0 +1,12 @@
+/*
+ * control.h
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: markp
+ */
+
+#ifndef APPLICATION_USER_CORE_INC_I2C_UTILS_H_
+#define APPLICATION_USER_CORE_INC_I2C_UTILS_H_
+
+
+#endif /* APPLICATION_USER_CORE_INC_I2C_UTILS_H_ */
