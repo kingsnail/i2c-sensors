fdbb99ea4619c6a9296267cbca30162033a82f46
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 18:55:17 +0100

    Create sensors.h

---
+++ inc/sensors.h
@@ -0,0 +1,13 @@
+/*
+ * sensors.h
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: markp
+ */
+
+#ifndef APPLICATION_USER_CORE_INC_SENSORS_H_
+#define APPLICATION_USER_CORE_INC_SENSORS_H_
+
+void* sensor_thread_function( void *argument );
+
+#endif /* APPLICATION_USER_CORE_INC_SENSORS_H_ */
