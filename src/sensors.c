81c82ad3fbf6e7dc3dc5d2f7dd11d11f83481dfc
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 18:55:59 +0100

    Create sensors.c

---
+++ src/sensors.c
@@ -0,0 +1,20 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "globals.h"
+
+#define INTERVAL_MS 500
+
+void* sensor_thread_function(void* arg) {
+
+    int lastFrameCounter = 0;
+  
+    while(1) {
+        if ( frameCounter != lastFrameCounter ) {
+            lastFrameCounter = frameCounter;
+            printf("Sense\n");
+        }
+    }
+    return NULL;
+}
