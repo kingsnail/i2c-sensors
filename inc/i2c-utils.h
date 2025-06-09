094ccc9043f4dd6838d81f396547514bc3863a3a
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 22:03:06 +0100

    Update i2c-utils.h

--- inc/i2c-utils.h
+++ inc/i2c-utils.h
@@ -8,5 +8,8 @@
 #ifndef APPLICATION_USER_CORE_INC_I2C_UTILS_H_
 #define APPLICATION_USER_CORE_INC_I2C_UTILS_H_
 
+uint8_t readRegister( uint8_t addr, uint8_t reg );
+
+uint8_t writeRegister( uint8_t addr, uint8_t reg, uint8_t val )
 
 #endif /* APPLICATION_USER_CORE_INC_I2C_UTILS_H_ */
