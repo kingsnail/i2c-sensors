bee00a6ca5d2044d12cdcaea4bbf3bb6fd892088
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 19:38:25 +0100

    Update control.c

--- src/control.c
+++ src/control.c
@@ -9,11 +9,51 @@
 void* control_thread_function(void* arg) {
 
     int lastFrameCounter = 0;
-  
+    int waitCount        = 0;
+    
     while(1) {
         if ( frameCounter != lastFrameCounter ) {
             lastFrameCounter = frameCounter;
             printf("Tock\n");
+            switch ( systemState ) {
+
+  			    case SYS_STATE_READY:
+                       break;
+
+			    case SYS_STATE_ERROR:
+				       waitCount = 0;
+				       systemState == SYS_STATE_WAIT;
+                       break;
+
+			    case SYS_STATE_RUN:
+			    	   //processFrame();
+			    	   break;
+
+			       case SYS_STATE_CALIB:
+					   if ( calibDone == 0) {
+	  				       //calibrateSensors();
+					   } else {
+						   systemState = SYS_STATE_RUN;
+					   }
+                       break;
+
+			       case SYS_STATE_INIT:
+			    	   if ( initDone == 1 ) {
+			    		   systemState = SYS_STATE_CALIB;
+			    	   }
+                       break;
+
+			       case SYS_STATE_WAIT:
+					   waitCount++;
+					   if (waitCount > 20) {
+						   systemState = SYS_STATE_READY;
+					   }
+                       break;
+
+			       default:
+			    	   systemState = SYS_STATE_ERROR;
+			   }
+
         }
     }
     return NULL;
