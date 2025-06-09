dabf1b17e68a18d6c23aacabb2fa21dda0aa615b
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 18:17:16 +0100

    Create timer.h

---
+++ inc/timer.h
@@ -0,0 +1,13 @@
+/*
+ * task_timer.h
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: markp
+ */
+
+#ifndef APPLICATION_USER_CORE_INC_TIMER_H_
+#define APPLICATION_USER_CORE_INC_TIMER_H_
+
+void timer_thread_function( void *argument );
+
+#endif /* APPLICATION_USER_CORE_INC_TIMER_H_ */
