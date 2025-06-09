44ef212c338a8df37e28fc432dd33cf92218d64b
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 18:32:08 +0100

    Create control.h

---
+++ inc/control.h
@@ -0,0 +1,13 @@
+/*
+ * control.h
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: markp
+ */
+
+#ifndef APPLICATION_USER_CORE_INC_CONTROL_H_
+#define APPLICATION_USER_CORE_INC_CONTROL_H_
+
+void* control_thread_function( void *argument );
+
+#endif /* APPLICATION_USER_CORE_INC_CONTROL_H_ */
