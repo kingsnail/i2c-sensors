fbfded46485e0365dff0d3ac34996ac155543844
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 18:31:15 +0100

    Create control.c

---
+++ src/control.c
@@ -0,0 +1,19 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "globals.h"
+
+#define INTERVAL_MS 500
+
+void* control_thread_function(void* arg) {
+
+    int last_FrameCounter = 0;
+  
+    while(1) {
+      if ( frameCounter != lastFrameCounter ) {
+          lastFrameCounter = frameCounter;
+          printf("Tock\n");
+      }
+    return NULL;
+}
