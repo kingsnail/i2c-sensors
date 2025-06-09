52c6d7c0e749ce42d1419636ef2d3370bcf1a425
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-09 22:15:41 +0100

    Update sensors.c

--- src/sensors.c
+++ src/sensors.c
@@ -2,10 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
+
 #include "globals.h"
 #include "i2c-utils.h"
 
 #define INTERVAL_MS 500
+
+void initCompass( void ) {
+    
+	uint8_t chipidA = ReadRegister(HMC5883L_ADDR, HMC5883L_REG_IR_A);
+	uint8_t chipidB = ReadRegister(HMC5883L_ADDR, HMC5883L_REG_IR_B);
+	uint8_t chipidC = ReadRegister(HMC5883L_ADDR, HMC5883L_REG_IR_C);
+	/printf("HMC5883L ID=%2x%2x%2x\r\n", chipidA, chipidB, chipidC);
+
+	WriteRegister(HMC5883L_ADDR,
+			       HMC5883L_REG_MR,
+	 			   HMC5883L_MR_HS_OFF | HMC5883L_MR_MODE_SINGLE_M
+	             );
+
+	uint8_t mode = ReadRegister(HMC5883L_ADDR, HMC5883L_REG_MR);
+	printf("Mode = %2x\r\n", mode);
+
+	WriteRegister(HMC5883L_ADDR,
+	 		       HMC5883L_REG_MR,
+	 			   HMC5883L_MR_HS_OFF | HMC5883L_MR_MODE_CONT_M
+                 );
+
+	mode = ReadRegister(HMC5883L_ADDR, HMC5883L_REG_MR);
+	printf("Mode = %2x\r\n", mode);
+
+}
+
+void initIMU( void ) {
+
+}
+
+void initDisplay( void ) {
+
+}
 
 void* sensor_thread_function(void* arg) {
 
@@ -22,7 +57,9 @@ void* sensor_thread_function(void* arg) {
                     break;
                 
                 case SYS_STATE_INIT :
-                    // Do init tasks
+                    initCompass();
+                    initIMU();
+                    initDisplay();
                     initDone = 1; 
                     break;
                 
