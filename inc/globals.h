f0c97fbaab8618a19f37c1b70b24ae0bb9ac3ad0
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-11 23:03:06 +0100

    Update globals.h

--- inc/globals.h
+++ inc/globals.h
@@ -18,12 +18,14 @@ extern pthread_mutex_t i2cBusLock;
 #define SYS_STATE_READY    ( 1 )
 #define SYS_STATE_INIT     ( 2 )
 #define SYS_STATE_CALIB    ( 3 )
-#define SYS_STATE_RUN      ( 4 )
-#define SYS_STATE_ERROR    ( 5 )
-#define SYS_STATE_WAIT     ( 6 )
+#define SYS_STATE_ORIENT   ( 4 )
+#define SYS_STATE_RUN      ( 5 )
+#define SYS_STATE_ERROR    ( 6 )
+#define SYS_STATE_WAIT     ( 7 )
 
 #define MASTER_IT_RATE_HZ  ( 20 )
 #define CALIBRATE_FRAMES   ( 500 )
+#define ORIENTATE_FRAMES   ( 200 )
 #define ERROR_WAIT_FRAMES  ( 20 )
 
 extern volatile int systemState;
