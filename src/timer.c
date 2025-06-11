b95f86dfacb7eb7941d373834178db8bca2a8a0a
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-11 12:39:50 +0100

    Update timer.c

--- src/timer.c
+++ src/timer.c
@@ -5,13 +5,15 @@
 #include <time.h>
 #include "globals.h"
 
-#define INTERVAL_MS 200
+#define INTERVAL_MS 100
 
 void* timer_thread_function(void* arg) {
     struct timespec ts;
     ts.tv_sec = 0;
     ts.tv_nsec = INTERVAL_MS * 1000000L;  // Convert ms to ns
 
+    struct timespec start, end;
+	
     while(1) {
 	if ( frameCounter % 20 == 0 ) {
 	    printf("C:%0.3fms S:%0.3fms\n", controlFrameTimeMs, sensorFrameTimeMs);
