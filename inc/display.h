fc1cbfce4cbcec652ada20c5749aeaabcd597fde
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-13 12:44:42 +0100

    Create display.h

---
+++ inc/display.h
@@ -0,0 +1,14 @@
+
+/*
+ * dispay.h
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: markp
+ */
+
+#ifndef APPLICATION_USER_CORE_INC_DISPLAY_H_
+#define APPLICATION_USER_CORE_INC_DISPLAY_H_
+
+void* display_thread_function( void *argument );
+
+#endif /* APPLICATION_USER_CORE_INC_DISPLAY_H_ */
