7c6aebc7366c39c7328b7264b6a70ca0453081ad
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-21 15:46:17 +0100

    Update oled-utils.c

--- src/oled-utils.c
+++ src/oled-utils.c
@@ -3,6 +3,15 @@
 #include "stdlib.h"
 #include "oledfont.h"  	 
 
+#define SIZE 8
+#define XLevelL		0x00
+#define XLevelH		0x10
+#define Max_Column	128
+#define Max_Row		64
+#define	Brightness	0xFF 
+#define X_WIDTH 	128
+#define Y_WIDTH 	64	    						  
+
 #define OLED_DATA ( 1 == 0 )
 #define OLED_CMD  ( 1 == 1 )
 
