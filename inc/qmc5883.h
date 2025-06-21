a1f0bc03440946f1f2c305a580862f1a0ceead61
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-21 21:57:38 +0100

    Create qmc5883.h

---
+++ inc/qmc5883.h
@@ -0,0 +1,29 @@
+#ifndef APPLICATION_USER_CORE_INC_QMC5883L_H_
+#define APPLICATION_USER_CORE_INC_QMC5883L_H_
+
+
+#define QMC5883L_ADDR          (0x0D)
+
+// Register Definitions
+#define QMC5883L_REG_X_LSB     (0x00)
+#define QMC5883L_REG_X_MSB     (0x01)
+
+#define QMC5883L_REG_Z_LSB     (0x02)
+#define QMC5883L_REG_Z_MSB     (0x03)
+
+#define QMC5883L_REG_Y_LSB     (0x04)
+#define QMC5883L_REG_Y_MSB     (0x05)
+
+#define QMC5883L_REG_STATUS    (0x06)
+
+#define QMC5883L_REG_TEMP_LSB  (0x07)
+#define QMC5883L_REG_TEMP_MSB  (0x08)
+
+#define QMC5883L_REG_CONTROL_1 (0x09)
+#define QMC5883L_REG_CONTROL_2 (0x0A)
+
+#define QMC5883L_REG_SR_PERIOD (0x0B)
+
+#define QMC5883L_REG_CHIP_ID   (0x0D)
+
+#endif /* APPLICATION_USER_CORE_INC_QMC5883L_H_ */
