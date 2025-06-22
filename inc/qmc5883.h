197a2b78dc71ea5e56791abc65cd64a1cb309e1e
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2025-06-22 11:04:36 +0100

    Update qmc5883.h

--- inc/qmc5883.h
+++ inc/qmc5883.h
@@ -19,7 +19,21 @@
 #define QMC5883L_REG_TEMP_LSB  (0x07)
 #define QMC5883L_REG_TEMP_MSB  (0x08)
 
-#define QMC5883L_REG_CONTROL_1 (0x09)
+#define QMC5883L_REG_CONTROL_1     (0x09)
+#define   QMC5883L_MODE_STANDBY    (0x00)
+#define   QMC5883L_MODE_CONTINUOUS (0x01)
+#define   QMC5883L_ODR_10HZ        (0x00)
+#define   QMC5883L_ODR_50HZ        (0x04)
+#define   QMC5883L_ODR_100HZ       (0x08)
+#define   QMC5883L_ODR_200HZ       (0x0C)
+#define   QMC5883L_RNG_2G          (0x00)
+#define   QMC5883L_RNG_8G          (0x10)
+#define   QMC5883L_OSR_512         (0x00)
+#define   QMC5883L_OSR_256         (0x40)
+#define   QMC5883L_OSR_128         (0x80)
+#define   QMC5883L_OSR_64          (0xC0)
+
+
 #define QMC5883L_REG_CONTROL_2 (0x0A)
 
 #define QMC5883L_REG_SR_PERIOD (0x0B)
